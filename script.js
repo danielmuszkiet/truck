5c44e2b40b8f4713ffb3037803a25612168849ba
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2024-05-16 12:41:45 +0200

    bug fix

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 var buttons = document.querySelectorAll(".buttonCall");
 var spans = document.querySelectorAll(".close");
 const form = document.getElementById("my-form");
+const heroFormBtn = document.getElementById("hero-contact-btn");
+
+heroFormBtn.addEventListener("click", () => {
+  const from = document.getElementById("from-date1").value;
+  const to = document.getElementById("to-date1").value;
+  const jeepType1 = document.getElementById("jepp-type1").value;
+
+  const from2 = document.getElementById("from-date2");
+  const to2 = document.getElementById("to-date2");
+  const jeepType2 = document.getElementById("jepp-type2");
+  from2.value = from;
+  to2.value = to;
+  jeepType2.value = jeepType1;
+});
 
 form.addEventListener("submit", async (event) => {
   console.log("Test");
@@ -15,9 +29,11 @@ form.addEventListener("submit", async (event) => {
   const tel = formData.get("tel");
   const jeepType = formData.get("jeep-select");
   const exp = formData.get("offroad-exp");
+  const yesno = formData.get("yesno");
+  console.log(pickUpDate, returnDate, email, msg, tel, jeepType, exp, yesno);
 
   const res = await fetch(
-    "https://truck-backend-kipy.onrender.com/send-email",
+    "https://truck-backend-znw3.onrender.com/send-email",
     {
       method: "post",
       headers: { "Content-Type": "application/json" },
@@ -29,18 +45,17 @@ form.addEventListener("submit", async (event) => {
         tel: tel,
         jeepType: jeepType,
         exp: exp,
+        yesno: yesno,
       }),
     }
   );
 
   if (res.ok) {
-    alert("Mail versendet! Sie werden nun weitergeleitet....");
-    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+    alert("TODO Bestätigung zeigen");
   } else {
-    console.log("Hopala");
+    console.log("Hopala etwas ist falsch gelaufen");
   }
 });
-
 
 // Öffnen des Modals bei Klick auf den Button
 buttons.forEach(function (button) {
